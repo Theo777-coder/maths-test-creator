2bb671197f333f91585fb1b3082acc41a6dc160d
theo <67960721+Theo777-coder@users.noreply.github.com>
2021-02-27 00:35:48 +0000

    Fixed getting the time of the test

--- model/getTime.php
+++ model/getTime.php
@@ -5,11 +5,12 @@ include_once 'tests.php';
 session_start();
 if (isset($_SESSION["username"])) {
     global $pdo;
-    $statement = $pdo->prepare("SELECT testTime FROM  db_name.tests WHERE  userID = (SELECT users.id FROM db_name.users WHERE users.username = ?)
+    $statement = $pdo->prepare("SELECT testTime FROM  db_name.tests WHERE  userID = (SELECT users.id FROM db_name.users WHERE username = ?)
     ORDER  BY id DESC LIMIT  1");
     $statement->bindValue(1, $_SESSION["username"], PDO::PARAM_STR);
     $statement->execute();
     $results = $statement->fetchAll(PDO::FETCH_CLASS, "tests");
+    echo json_encode($results[0]->testtime);
 }
 if (!isset($_SESSION["username"])) {
     $origin = date_create($_SESSION["testStartTime"]);
