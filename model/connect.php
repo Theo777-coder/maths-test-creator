02964ea51beef4ca7d17f1af25d0c4c17004c178
theo <67960721+Theo777-coder@users.noreply.github.com>
2021-03-22 20:16:24 +0000

    Fix

--- model/connect.php
+++ model/connect.php
@@ -1,16 +1,16 @@
 <?php
-// $db = parse_url(getenv("DATABASE_URL"));
+$db = parse_url(getenv("DATABASE_URL"));
 
-// $pdo = new PDO("pgsql:" . sprintf(
-//     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-//     $db["host"],
-//     $db["port"],
-//     $db["user"],
-//     $db["pass"],
-//     ltrim($db["path"], "/")
-// ));
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
 
 //localhost login
-$dsn = "pgsql:host=localhost;port=5432;dbname=postgres;user=postgres;password=postgres";
-$pdo = new PDO($dsn);
+// $dsn = "pgsql:host=localhost;port=5432;dbname=postgres;user=postgres;password=postgres";
+// $pdo = new PDO($dsn);
 ?>
