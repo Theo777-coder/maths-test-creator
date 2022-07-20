251c5a7446906e1772320383b968e0f1b76c726c
sweetie <67960721+Theo777-coder@users.noreply.github.com>
2022-07-20 18:00:06 +0100

    fixing wrong JS and CSS links

--- view/tests_view.php
+++ view/tests_view.php
@@ -13,7 +13,7 @@ require_once "topBar_view.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="../AJAX/tests.js"></script>
+    <!-- <script src="../AJAX/tests.js"></script> -->
     <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 </head>
 
